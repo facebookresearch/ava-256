b674414d85bbbeab6513ab6a6c72c24828b2f02e
julietamartinez <julietamartinez@meta.com>
2024-06-08 08:06:37 -0700

    point to newer checkpoints

--- sbatch-256.sh
+++ sbatch-256.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=learn
-#SBATCH --time=1-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --job-name=AVA256_TRAIN
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --gpus-per-node=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-gpu=32
@@ -10,6 +10,7 @@
 #SBATCH --gpus-per-task=8
 #SBATCH --output=/home/%u/rsc/ava-256/logs/slurm-%j.out
 #SBATCH --error=/home/%u/rsc/ava-256/logs/slurm-%j.err
+#SBATCH --qos=urgent_deadline
 
 source /uca/conda-envs/activate-latest
 
@@ -23,4 +24,4 @@ export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
 # export NCCL_DEBUG=INFO
 
 cd /home/$USER/rsc/ava-256/ || exit 1
-srun python ddp-train.py --masteraddress ${MASTER_ADDR} --masterport ${MASTER_PORT} --config configs/config-4.yaml
+srun python ddp-train.py --masteraddress ${MASTER_ADDR} --masterport ${MASTER_PORT} --config configs/config-256.yaml
