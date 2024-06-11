b481d0a96ed156143b88a3dd3fb0050c2f18cfb7
leochli <leochli@devvm24038.atn0.facebook.com>
2024-06-11 09:17:49 -0700

    Interface change to support: 1. decode and render only 2. alpha mask in the uv space

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -14,6 +14,7 @@ import einops
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from extensions.utils.utils import compute_raydirs
 from models.raymarchers.mvpraymarcher import Raymarcher
@@ -73,6 +74,7 @@ class Autoencoder(nn.Module):
         residuals_weight: float = 1.0,
         output_set: Set[str] = set(),
         force_neutral=False,
+        alpha_mask: Optional[torch.Tensor] = None,
     ) -> Dict[str, Optional[torch.Tensor]]:
         """
         Params
@@ -86,6 +88,7 @@ class Autoencoder(nn.Module):
 
             camindex : torch.Tensor[int32], optional [B] Camera index within the list of all cameras
             pixelcoords : [B, H', W', 2] Pixel coordinates to render of the target view camera
+            alpha_mask: [U, V] Alpha mask appied to the primitives in the UV space
 
             validinput : torch.Tensor, optional [B] Whether the current batch element is valid (used for missing images)
             fixedcamimage : torch.Tensor, optional [B, 3, 512, 334] Camera images from a one or more cameras that are always the same (i.e., unrelated to target)
@@ -109,7 +112,11 @@ class Autoencoder(nn.Module):
 
         # Return {z_tex, b_tex, z_geo, b_geo}
         # Step 0. Get identity encoding
-        id_cond = self.id_encoder(target_neut_verts, target_neut_avgtex)
+        if id_cond is None:
+            id_cond = self.id_encoder(target_neut_verts, target_neut_avgtex)
+        else:
+            assert target_neut_avgtex is not None, "Must provide target_neut_avgtex or id_cond"
+            assert target_neut_verts is not None, "Must provide target_neut_verts or id_cond"
 
         # Step 1. Get expression encoding
         expr_code = self.expr_encoder(
@@ -126,20 +133,101 @@ class Autoencoder(nn.Module):
 
         # NOTE(julieta) expr_mu and expr_logstsd are used to compute kl divergence
 
+        # Update encoder outputs
+        encout = {
+            # === Returned every time ===
+            "encoding": expr_code,
+            "expr_mu": expr_mu,
+            "expr_logstd": expr_logstd,
+            # === Returned if asked for, useful for debugging and visualization ===
+            "id_cond": id_cond if "idcond" in output_set else None,
+        }
+        resultout.update(encout)
+
+        # Step2: Decode and Render from expr_code and id_cond
+        decout = self.decode(
+            camrot=camrot,
+            campos=campos,
+            focal=focal,
+            princpt=princpt,
+            modelmatrix=modelmatrix,
+            id_cond=id_cond,
+            expr_encoding=expr_code,
+            pixelcoords=pixelcoords,
+            idindex=idindex,
+            camindex=camindex,
+            bg=bg,
+            running_avg_scale=running_avg_scale,
+            gt_geo=gt_geo,
+            residuals_weight=residuals_weight,
+            output_set=output_set,
+            alpha_mask=alpha_mask,
+        )
+
+        # Update decoder outputs
+        resultout.update(decout)
+
+        return resultout
+
+
+    def decode(
+        self,
+        # Camera parameters
+        camrot: torch.Tensor,
+        campos: torch.Tensor,
+        focal: torch.Tensor,
+        princpt: torch.Tensor,
+        modelmatrix: torch.Tensor,
+        # Encoder inputs: ID Encoder & Expr Encoder
+        id_cond: dict,
+        expr_encoding: torch.Tensor,
+        # Select pixels to evalaute ray marching on
+        pixelcoords: torch.Tensor,
+        # Indexing for background/color modeling
+        idindex: Optional[torch.Tensor] = None,
+        camindex: Optional[torch.Tensor] = None,
+        bg: Optional[torch.Tensor] = None,
+        running_avg_scale: bool = False,
+        gt_geo: Optional[torch.Tensor] = None,
+        residuals_weight: float = 1.0,
+        output_set: Set[str] = set(),
+        alpha_mask: Optional[torch.Tensor] = None,
+    ) -> Dict[str, Optional[torch.Tensor]]:
+        """
+        Method to only run the decoder and renderer
+        """
         # compute relative viewing position
         # NOTE(julieta) should we be passing viewdir instead of pos to the decoder?
         # viewrot = torch.bmm(camrot, modelmatrix[:, :3, :3])
         viewpos = torch.bmm((campos[:, :] - modelmatrix[:, :3, 3])[:, None, :], modelmatrix[:, :3, :3])[:, 0, :]
         # viewdir = viewpos / torch.sqrt(torch.sum(viewpos**2, dim=-1, keepdim=True))
 
+        # Decoding Step 1. Get decoder output
         decout = self.decoder_assembler(
             id_cond,
-            expr_code,
+            expr_encoding,
             viewpos,
             running_avg_scale,
             gt_geo,
             residuals_weight,
         )
+
+        # Apply alpha mask to the primitives
+        if alpha_mask is not None:
+            nh = int(np.sqrt(self.decoder_assembler.nprims))
+            alpha_mask_strided = F.interpolate(
+                alpha_mask.unsqueeze(0).unsqueeze(0), 
+                size=(nh, nh),
+                antialias=False
+            )[0][0]
+            valid_prims = alpha_mask_strided.reshape(-1).bool()
+            assert valid_prims.shape[0] == decout["template"].shape[1], f"valid_prims: {valid_prims.shape}, template: {decout['template'].shape}"
+            decout["template"] = decout["template"][:, valid_prims].contiguous()
+            decout["primpos"] = decout["primpos"][:, valid_prims].contiguous()
+            decout["primrot"] = decout["primrot"][:, valid_prims].contiguous()
+            decout["primscale"] = decout["primscale"][:, valid_prims].contiguous()
+            if "warp" in decout:
+                decout["warp"] = decout["warp"][:, valid_prims].contiguous()
 
         # TODO(julieta) check whether this makes sense
         samplecoords = torch.cat(
@@ -155,8 +243,8 @@ class Autoencoder(nn.Module):
             campos, camrot, focal, princpt, pixelcoords, self.raymarcher.volume_radius
         )
 
+        # Decoding Step 2. Get decoder output
         # Ray march across the ray directions to see the output image
-
         rayrgb, rayalpha, _, pos_img = self.raymarcher(
             raypos,
             raydir,
@@ -165,16 +253,16 @@ class Autoencoder(nn.Module):
             with_pos_img=("pos_img" in output_set),
         )
 
-        # color correction
+        # [Optional] Decoding Step 3. color correction
         if (self.colorcal is not None) and (camindex is not None) and (idindex is not None):
             rayrgb = self.colorcal(rayrgb, camindex, idindex)
 
-        # 4. Decode the background
-        if bg is None and self.bgmodel is not None:
+        # [Optional] Decoding Step 4. Decode the background
+        if bg is None and (self.bgmodel is not None and camindex is not None and idindex is not None):
             # TODO(julieta) raise an error if bg is None and either camidx or idindex are None
             bg = self.bgmodel(camindex, idindex, samplecoords)
 
-        # 5. matting
+        # Decoding Step 5. matting
         if bg is not None:
             rayrgb = rayrgb + (1.0 - rayalpha) * bg
         else:
@@ -183,19 +271,16 @@ class Autoencoder(nn.Module):
             colour = np.asarray(black, dtype=np.float32)
             rayrgb = rayrgb + (1.0 - rayalpha) * torch.from_numpy(colour).to("cuda")[None, :, None, None]
 
-        resultout = {
+        decout = {
             # === Returned every time ===
-            "encoding": expr_code,
-            "expr_mu": expr_mu,
-            "expr_logstd": expr_logstd,
             "irgbrec": rayrgb,
             "verts": decout["verts"],
             # === Returned if asked for, useful for debugging and visualization ===
             "primscale": decout["primscale"] if "primscale" in output_set else None,
-            "id_cond": id_cond if "idcond" in output_set else None,
             "samplecoords": samplecoords if "samplecooords" in output_set else None,
             "pos_img": pos_img if "pos_img" in output_set else None,
             "bg": bg if "bg" in output_set else None,
             "ialpha": rayalpha if "ialpha" in output_set else None,
         }
-        return resultout
+
+        return decout
