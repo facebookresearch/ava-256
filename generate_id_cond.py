f550ac94e6fb5ff714d3f24df0864c6c8c41fec6
leochli <leochli@devvm24038.atn0.facebook.com>
2024-06-11 10:38:43 -0700

    Format

--- generate_id_cond.py
+++ generate_id_cond.py
@@ -8,19 +8,13 @@ import argparse
 import pathlib
 from typing import Dict, Union
 
-import einops
 import torch
 import yaml
 
-from data.ava_dataset import (
-    MultiCaptureDataset as AvaMultiCaptureDataset,
-    none_collate_fn,
-    SingleCaptureDataset as AvaSingleCaptureDataset,
-)
-from data.utils import MugsyCapture
+from data.ava_dataset import MultiCaptureDataset as AvaMultiCaptureDataset
 from fvcore.common.config import CfgNode as CN
 from tqdm import tqdm
-from utils import get_autoencoder, load_checkpoint, render_img, tocuda, train_csv_loader
+from utils import get_autoencoder, load_checkpoint, tocuda, train_csv_loader
 
 
 if __name__ == "__main__":
@@ -28,7 +22,9 @@ if __name__ == "__main__":
     This is a script to generate and dump the id conditioning outputs for all identities. For downstream
     tasks where the universal decoder is already trained, it would be easier to use the cached identity conditioning outputs.
     """
-    parser = argparse.ArgumentParser(description="Generate id conditioning outputs for all identities")
+    parser = argparse.ArgumentParser(
+        description="Generate id conditioning outputs for all identities"
+    )
     parser.add_argument(
         "--checkpoint",
         type=str,
@@ -36,7 +32,10 @@ if __name__ == "__main__":
         help="checkpoint location",
     )
     parser.add_argument(
-        "--output-dir", type=str, default="identity_conditioning/", help="output directory"
+        "--output-dir",
+        type=str,
+        default="identity_conditioning/",
+        help="output directory",
     )
     parser.add_argument(
         "--config", default="configs/config.yaml", type=str, help="config yaml file"
@@ -52,11 +51,7 @@ if __name__ == "__main__":
 
     train_params = config.train
 
-    output_dir = (
-        args.output_dir
-        + "/"
-        + "identity_conditioning"
-    )
+    output_dir = args.output_dir + "/" + "identity_conditioning"
 
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
@@ -86,17 +81,19 @@ if __name__ == "__main__":
     texstd = dataset.texstd
     vertstd = dataset.vertstd
 
-    for single_capture, single_dataset in tqdm(dataset.single_capture_datasets.items(), desc="Captures"):
+    for single_capture, single_dataset in tqdm(
+        dataset.single_capture_datasets.items(), desc="Captures"
+    ):
         # Get neutral
         neut_avgtex, neut_vert = single_dataset.neut_avgtex, single_dataset.neut_vert
         # Normalize
-        neut_avgtex=(neut_avgtex - texmean) / texstd
-        neut_verts=(neut_vert - vertmean) / vertstd
+        neut_avgtex = (neut_avgtex - texmean) / texstd
+        neut_verts = (neut_vert - vertmean) / vertstd
         data = {
             "neut_avgtex": torch.from_numpy(neut_avgtex[None]),
             "neut_verts": torch.from_numpy(neut_verts[None]),
         }
-        
+
         cuda_data: Dict[str, Union[torch.Tensor, int, str]] = tocuda(data)
 
         # Generate id_cond from the capture
@@ -107,8 +104,9 @@ if __name__ == "__main__":
         save_dir = output_dir + "/" + ident_str
 
         pathlib.Path(save_dir).mkdir(parents=True, exist_ok=True)
-        
+
         torch.save(id_cond, f"{save_dir}/id_cond.pkl")
 
-
-    print(f"Done! Dumped {len(dataset.single_capture_datasets)} id_cond to {output_dir}")
+    print(
+        f"Done! Dumped {len(dataset.single_capture_datasets)} id_cond to {output_dir}"
+    )
